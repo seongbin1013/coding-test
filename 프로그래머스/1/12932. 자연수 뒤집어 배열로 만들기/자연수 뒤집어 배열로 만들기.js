49cc3ff13ef8b19437cb63f782ee765c53bc0c7e
seongbin1013 <104897081+seongbin1013@users.noreply.github.com>
2024-05-02 09:16:59 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let arr = n.toString().split('');
+    let answer = [];
+
+    for(let i=arr.length-1; i>=0; i--){
+        answer.push(Number(arr[i]));
+    }
+  return answer;
+}
