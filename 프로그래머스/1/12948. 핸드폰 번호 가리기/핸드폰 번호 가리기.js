67e40ea584af16987392ba55009879049258301e
seongbin1013 <104897081+seongbin1013@users.noreply.github.com>
2024-10-16 17:32:08 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,4 +1,4 @@
 function solution(phone_number) {
-    const str = phone_number.slice(0,-4)
-    return phone_number.replace(str, "*".repeat(str.length))
+    let number = phone_number.slice(0,-4); // slice 매서드: 첫글자 부터 마지막 4글자를 뺀 모든 글자
+    return phone_number.replace(number,"*".repeat(number.length)) // replace 매서드: number 대신 "*"로 변경. repeat 매서드: 문자열을 주어진 횟수만큼 반복하여 새로운 문자열을 생성함.
 }
