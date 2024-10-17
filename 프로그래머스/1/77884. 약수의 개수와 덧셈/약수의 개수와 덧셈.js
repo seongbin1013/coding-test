f20d017f1e0c78222aef44de3e43a69daf92800d
seongbin1013 <104897081+seongbin1013@users.noreply.github.com>
2024-10-17 20:07:08 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,11 +1,11 @@
 function solution(left, right) {
-    let answer = 0
-    for(let i = left; i <= right; i++ ) {
-    if (Number.isInteger(Math.sqrt(i))) {
-        answer -= i
-    } else {
-        answer += i
+    let answer = 0;
+    for (let i = left; i <= right; i++) { // left 부터 right 까지 반복
+        if (Math.sqrt(i) % 1 === 0) { // 약수가 홀수인 매개변수를 찾을려면 제곱수를 구하면 됨.
+            answer -= i; // 약수가 홀수일때 -
+        } else {
+            answer += i; // 짝수일떄 +
+        } 
     }
-}
-    return answer
+    return answer;
 }
